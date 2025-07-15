c03014cd617e5139acd60398b3e0322a0b2f95c2
NizarNaser <nizarnaser17@gmail.com>
2025-07-15 18:21:46 +0200

    new commet

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['lh3.googleusercontent.com'],
+      domains: ['0d6gifolc5gc3tzu.public.blob.vercel-storage.com'],
     },
   }
 
